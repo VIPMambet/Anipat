73cc93d049e846e62f3a8eb3a033185695493332
Yrysdaulet <yrysdauletmadiev@gmail.com>
2025-04-16 18:03:52 +0500

    2 - Часть Есть: валидация , Mainmenu , Mobilemenu Создал коннтроллер , предстваления About Использовал модель Message , строготиризированный модель Есть Layout , LayoutBase

--- Anipat/Controllers/HomeController.cs
+++ Anipat/Controllers/HomeController.cs
@@ -2,10 +2,8 @@ using System.Diagnostics;
 using Anipat.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Anipat.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace Anipat.Controllers {
+    public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -17,9 +15,29 @@ namespace Anipat.Controllers
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Contact()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(Message usermessage)
+        {
+            //if (string.IsNullOrWhiteSpace(usermessage.name))
+            //{
+            //    ModelState.AddModelError("name", "Поля обязательно для заполнение ");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+
+            else
+            {
+                return View("Contact", usermessage);
+            }
         }
 
     }
---
+++ Anipat/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;    
+
+namespace Anipat.Models {
+    public class Message {
+
+        [Required(ErrorMessage = "Поле name обязательно для заполнение")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Поле emial обязательно для заполнение")]
+        [EmailAddress(ErrorMessage = "Некорректный email")]
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+
+
+    }
+}
